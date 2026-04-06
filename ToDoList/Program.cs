cdb619cb5309de179417ac63ad13979d3e4d69b0
saipriyak2 <priyakaja76@gmail.com>
2026-04-06 07:11:46 -0400

    Re-add todolist

---
+++ ToDoList/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the To Do List Program!");
+            List<string> taskList = new List<string>();
+            string option = "";
+
+            while (option != "e")
+            {
+                Console.WriteLine("\n What would you like to do?");
+                Console.WriteLine("Enter 1 to add a task to the list");
+                Console.WriteLine("Enter 2 to remove a task from the list");
+                Console.WriteLine("Enter 3 to view the list");
+                Console.WriteLine("Enter e to exit the program\n");
+
+                option = Console.ReadLine();
+
+                if (option == "1")
+                {
+                    Console.WriteLine("Please add name of the task to the list");
+                    string task = Console.ReadLine();
+                    taskList.Add(task);
+                    Console.WriteLine("Task added to the list");
+                }
+                else if (option == "2")
+                {
+                    for (int i = 0; i < taskList.Count; i++)
+                    {
+                        Console.WriteLine(i + ":" + taskList[i]);
+                    }
+
+                    Console.WriteLine("Please enter the number of the task to remove");
+                    int taskNumber = Convert.ToInt32(Console.ReadLine());
+                    taskList.RemoveAt(taskNumber);
+
+                }
+                else if (option == "3")
+                {
+                    Console.WriteLine("The task in the to do list:");
+                    for (int i = 0; i < taskList.Count; i++)
+                    {
+                        Console.WriteLine(taskList[i]);
+                    }
+
+                }
+                else if (option == "e")
+                {
+                    Console.WriteLine("Exiting the program");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Option,Please try again");
+                }
+            }
+            Console.WriteLine("Thank you for using the To Do List program.");
+            Console.ReadLine();
+
+        }
+    }
+            
+}
+    
+
+
